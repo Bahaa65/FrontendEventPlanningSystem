16f2ff4bb6f633a9be18e998cbc2812f066367ba
janassalem <janamohamed248@gmail.com>
2025-12-05 12:53:33 +0200

    Front

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app';
-import { provideHttpClient, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { AuthInterceptor } from './app/services/auth.interceptor';
-import { importProvidersFrom } from '@angular/core';
 
-
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideHttpClient(),
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: AuthInterceptor,
-      multi: true
-    }
-  ]
-});
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
